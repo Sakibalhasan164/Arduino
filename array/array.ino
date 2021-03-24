8c9bf1756187377fc264023af0beea9840b0d008
sakib hasan <sakib686822@gmail.com>
2021-03-24 22:42:20 +0600

    added a function

--- array/array.ino
+++ array/array.ino
@@ -3,6 +3,11 @@ int d;
 //want to control delay with the potentiometer
 int pot=A0;
 
+int getDelay(){
+  int potValue=analogRead(pot);//read the voltate
+  int customDelay=map(potValue,0,1023,90,600);//map it to some amount of milisecond
+  return customDelay;
+  }
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -14,26 +19,20 @@ void setup() {
 void loop() {
 
 for(int i=0;i<5;i++){
-  int potValue=analogRead(pot);
-int customDelay=map(potValue,0,1023,50,600);
-//Serial.print(customDelay);
-d=customDelay;
+ d=getDelay();
 int index=pinArray[i];
   digitalWrite(index,HIGH);
   digitalWrite(index-1,LOW);
   delay(d);
   }
 for(int j=5;j>-1;j--){
-  int potValue=analogRead(pot);
-int customDelay=map(potValue,0,1023,50,600);
-//Serial.print(customDelay);
-d=customDelay;
+ d=getDelay();
   int index=pinArray[j];
   digitalWrite(index,LOW);
   digitalWrite(index-1,HIGH);
 //  Serial.println(pinArray[j]);
   delay(d);
   }
-
-  
 }
+
+//a function that will return delay
